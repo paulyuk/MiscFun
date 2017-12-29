48f8071849c9948d2f3562b71229218d237d9c3d
Paul Yuknewicz <paulyuk@microsoft.com>
2017-12-29 00:35:07 -0800

    Initial checkin of sendkeys code

---
+++ SendKeysToMinecraft/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 57);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Disable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(157, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mouse Bot Active";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 412);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Minecraft Keep Active Bot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SendKeysToMinecraft/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        bool _mouseEnabled = true;
+        bool firstrun = true;
+        const int MOUSE_MOVE_INTERVAL = 10000; //10 seconds by default
+
+        [DllImportAttribute("User32.dll")]
+
+        private static extern int FindWindow(String ClassName, String
+WindowName);
+
+        [DllImportAttribute("User32.dll")]
+        private static extern int SetForegroundWindow(int hWnd);
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.timer1.Interval = MOUSE_MOVE_INTERVAL;
+            this.timer1.Enabled = true; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.toggleMouseEnabled();
+        }
+
+        private void toggleMouseEnabled()
+        {
+            if (this._mouseEnabled) {
+                this._mouseEnabled = false;
+                this.label1.Text = "Mouse bot DISABLED";
+                this.button2.Text = "Enable";
+
+            } else {
+                this._mouseEnabled = true;
+                this.label1.Text = "Mouse bot ENABLED";
+                this.button2.Text = "Disable";
+            }
+
+        }
+
+        private void moveMouse()
+        {
+            // Set the Current cursor, move the cursor's Position,
+
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
+            Cursor.Position = new Point(Cursor.Position.X + 50, Cursor.Position.Y + 50);
+        }
+
+        private void doSendKeys()
+        {
+            //to activate an application
+            int hWnd = FindWindow(null, "Minecraft");
+            if (hWnd > 0)
+            {
+                SetForegroundWindow(hWnd);
+                System.Threading.Thread.Sleep(1000);
+
+                if (this.firstrun == true)
+                {
+                    SendKeys.Send("{Esc}");       //good to pause or unpause
+                    this.firstrun = false;
+                }
+                
+                SendKeys.Send("w");             //move forward
+                System.Threading.Thread.Sleep(1000);
+                SendKeys.Send("d");             //move right
+                System.Threading.Thread.Sleep(1000);
+               // SendKeys.Send("s");             //move back
+                System.Threading.Thread.Sleep(1000);
+               // SendKeys.Send("a");             //move left
+                System.Threading.Thread.Sleep(1000);
+
+
+            }
+        }
+
+        private void doheartbeateffect()
+        {
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Refresh();
+
+            System.Threading.Thread.Sleep(2000);
+
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this._mouseEnabled)
+            {
+                //this.moveMouse();
+                this.doSendKeys();
+
+                this.doheartbeateffect();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
